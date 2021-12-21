f23f0f9a42685583ab76df361ea3ec9880865593
Lioncat2002 <sankhayan2002@gmail.com>
2021-12-21 17:47:10 +0530

    db added

---
+++ web scraping/steam_unlocked/spider.py
@@ -0,0 +1,33 @@
+import requests
+import time
+import random
+from bs4 import BeautifulSoup as bs
+import json
+f=open('links.json',encoding="utf8")
+
+d={}
+d=json.load(f)
+f.close() 
+
+
+urls=["https://steamunlocked.net/category/sports/"]
+for url in urls:
+    for pages in range(1,11):
+        pg=f'{url}page/{pages}/'
+        r=random.randint(1,5)
+        time.sleep(r)
+        req = requests.get(pg)
+        soup = bs(req.text, 'html.parser')
+        print(f"Crawling... at page: {pages}, at link: {pg}")
+
+        for link in soup.findAll('div',{'class':'cover-item category'}):
+            a=link.find('a')
+            title=a.find('h1')
+            pgurl=a.get('href')
+            d[title.text]=a.get('href')
+            #print(d)
+            #f.write(f"Title: {title.text} \n Link: {a.get('href')} \n\n")
+
+with open("links.json",'w') as f:
+    json.dump(d,f)
+
