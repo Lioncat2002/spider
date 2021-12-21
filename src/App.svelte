8947314b354f41d883d6378979aae4b409c116aa
Lioncat2002 <sankhayan2002@gmail.com>
2021-12-21 20:11:33 +0530

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -5,22 +5,22 @@ let data={}
 onMount(async () => {
         const response = await fetch('https://raw.githubusercontent.com/Lioncat2002/spider/main/web%20scraping/links.json');
         const res = await response.json();
-		console.log(res)
         
 		data=res
-		console.log(data)
 		mounted=true
 
 		
 });
 
 </script>
-
+<h2>
+	Games:
+</h2>
 {#if mounted}
 <ol>
 	{#each Object.entries(data) as [title,link]}
 
-	<li><a href={link}>{title}</a></li>
+	<li> <h3><a href={link}>{title}</a></h3> </li>
 		
 	{/each}
 </ol>
